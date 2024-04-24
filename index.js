c2ce8382dc8d63b5ca5be54e05ab5cdca89461bd
Golam Mahabub Redoy <148676251+gmhridu@users.noreply.github.com>
2024-04-24 15:59:45 +0600

    first commit

--- index.js
+++ index.js
@@ -6,37 +6,30 @@ const router = require('./routes/coffee.route');
 const userRouter = require('./routes/user.route');
 const addProductRouter = require('./routes/addProduct.route');
 
-
-
-
-
 dotenv.config();
-// Define Port
 const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGODB_URI;
 
 const app = express();
-// middleware
-app.use(cors());
+app.use(cors({
+  origin: ["https://coffee-storess-server.vercel.app"],
+  methods: ["POST", "GET", "DELETE", "PUT", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json());
 
-// DB Connect
 ConnectDB(mongoUri);
 
-// routes
-app.use('/coffees', router)
-app.use('/users', userRouter)
-app.use('/addProducts', addProductRouter)
-
+app.use('/coffees', router);
+app.use('/users', userRouter);
+app.use('/addProducts', addProductRouter);
 
 app.use('*', (req, res) => {
   res.status(404).json({
     message: 'Page not found'
-  })
-})
+  });
+});
 
-
-// listen
 app.listen(port, () => {
-  console.log(`SIMPLE CRUD IS RUNNING ON PORT ${port}`)
-})
+  console.log(`SIMPLE CRUD IS RUNNING ON PORT ${port}`);
+});
